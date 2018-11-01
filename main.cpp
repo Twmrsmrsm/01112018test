58688b6d0cee161fc56a986235e566657da60429
PolinaPonomar <ponomareva.polia2013@yandex.ru>
2018-11-01 15:21:31 +0300

    01112018task2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cmath>
-
+//Выделить минимально необходимую память для хранения значения переменной: a = 1 b = 129 c = -32768 d = 1000.5
 
 int main () {
-    double i=1, t=60, phi=3, res;
-    res= pow(M_E,(-i)*t*sqrt(phi)+phi*pow(M_E, sqrt(phi)/i*t));
-    std::cout<< res << '\n';
-return 0;
+    bool a = 1;
+    unsigned char b = 129;
+    int c = -32768;
+    float d = 1000.5;
+    return 0;
 }
